08e58a7e2beb7ed048801d3661dc4568d6739759
Katherine Nguyen <khnguyen86@live.com>
2018-03-04 23:05:24 -0800

    added login and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   get 'main/index'
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
 
+  devise_for :users, controllers: { sessions: 'user/sessions' }
   root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
