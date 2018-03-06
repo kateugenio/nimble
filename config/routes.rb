e6b49d6411d58f6a97c6fb9f9017d206aa07569b
Katherine Nguyen <khnguyen86@live.com>
2018-03-05 22:44:39 -0800

    add migration to User added first_name, last_name, and company; updated signup and signin views, added validation to User model, updated registration controller sanitizer/strong params

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'main/index'
 
 
-  devise_for :users, controllers: { sessions: 'user/sessions' }
+  # devise_for :users, controllers: { registrations: 'user/registrations' }
+  devise_for :users, controllers: { registrations: 'user/registrations' }
   root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
