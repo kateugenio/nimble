35c9a8f9172a3acb394e895b6d75eaddd97b6073
Katherine Nguyen <khnguyen86@live.com>
2018-03-30 11:14:53 -0700

    adds user story show and delete

--- app/controllers/user_stories_controller.rb
+++ app/controllers/user_stories_controller.rb
@@ -1,5 +1,7 @@
 class UserStoriesController < ApplicationController
 
+	before_action :validate_user, only: [:show]
+
 	def create
 		@team = Team.find(params[:team_id])
 		@user_story = @team.user_stories.create(user_story_params)
@@ -11,10 +13,30 @@ class UserStoriesController < ApplicationController
 		end
 	end
 
+	def show
+		@team = Team.find(params[:team_id])
+		@user_story = @team.user_stories.find(params[:id])
+	end
+
+	def destroy
+		@team = Team.find(params[:team_id])
+		@user_story = @team.user_stories.find(params[:id])
+		@user_story.destroy
+		redirect_to team_path(@team)
+	end
+
 	private
 
 	def user_story_params
 		params.require(:user_story).permit(:title, :description, :acceptance_criteria, :estimate, :state, :priority, :order_id)
+	end
+
+	def validate_user
+		@team = Team.find(params[:team_id])
+		if current_user.company != @team.company
+	    	flash[:alert] = "You don't have access to this page!"
+	      	redirect_to root_path
+	    end
 	end
 
 end
