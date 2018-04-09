0ada5fd81c1b999dfd4eb96b330ac75419c437e5
Katherine Nguyen <khnguyen86@live.com>
2018-04-08 22:38:33 -0700

    updates production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,11 +101,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
+  address:              'smtp.sendgrid.net',
   port:                 587,
   domain:               'nimblealm.herokuapp.com',
-  user_name:            ENV["MAILER_USERNAME"],
-  password:             ENV["MAILER_PASSWORD"],
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
 
