715f2e0cda37bded3c9343ad8d2d19644117996a
Katherine Nguyen <khnguyen86@live.com>
2018-04-08 22:03:42 -0700

    updates production environment mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ end
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost:3000',
+  domain:               'nimblealm.herokuapp.com',
   user_name:            ENV["MAILER_USERNAME"],
   password:             ENV["MAILER_PASSWORD"],
   authentication:       'plain',
